d6da88f4ce73a71a8c3952ba8a5411d524c28c46
elayachiamritoff <elayachiamritoff@gmail.com>
2024-02-15 21:19:42 +0100

    Add build_tree method basics functionality

--- script.rb
+++ script.rb
@@ -6,4 +6,13 @@ class Tree
   def initialize(array)
     @root = build_tree(array)
   end
+
+
+  def build_tree(array)
+      unique_array = array.uniq.sort
+  end
 end
+
+
+arr = [1, 2, 3 , 1, 9, 5, 5, 1, 2, 9]
+data = Tree.new(arr)
